9dfe0403541d70e66021b6a43131137a6966c04e
kicchi <kicchidayo@gmail.com>
2018-07-27 16:54:31 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define INF 2000000000
+#define MOD 1000000007
+#define EPS (1e-10)
+
+using namespace std;
+
+
+vector<int>  dx = {1,-1,0,0};
+vector<int>  dy = {0,0,1,-1};
+
+
+int main() {
+
+	return 0;
+}
